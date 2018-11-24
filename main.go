7a654fe3c2289b69c89e34d99a28bc9c068c77a3
Du Yuyang <du.yuyang@rea-group.com>
2018-11-24 17:20:37 +1100

    get db working

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -13,6 +12,8 @@ import (
 
 	"encoding/json"
 	"encoding/xml"
+
+	"github.com/codegangsta/negroni"
 )
 
 type Page struct {
@@ -27,11 +28,23 @@ type SearchResult struct {
 	ID     string `xml:"owi,attr"`
 }
 
+var db *sql.DB
+
+func verifyDatabase(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	if err := db.Ping(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	next(w, r)
+}
+
 func main() {
 	t := template.Must(template.ParseFiles("templates/index.html"))
-	db, _ := sql.Open("sqlite3", "dev.db")
+	db, _ = sql.Open("sqlite3", "dev.db")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := Page{Name: "Gopher"}
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
@@ -43,7 +56,7 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
 
 		var results []SearchResult
 		var err error
@@ -58,30 +71,77 @@ func main() {
 
 	})
 
-	fmt.Println(http.ListenAndServe(":8080", nil))
+	mux.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
+		var book ClassifyBookResponse
+		var err error
+		if book, err = find(r.FormValue("id")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		_, err = db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)", nil, book.BookData.Title, book.BookData.Author, book.BookData.ID,
+			book.Classification.MostPopular)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	n := negroni.Classic()
+	n.Use(negroni.HandlerFunc(verifyDatabase))
+	n.UseHandler(mux)
+	n.Run(":8080")
+	//fmt.Println(http.ListenAndServe(":8080", nil))
 }
 
 type ClassifySearchResponse struct {
 	Results []SearchResult `xml:"works>work"`
 }
 
+type ClassifyBookResponse struct {
+	BookData struct {
+		Title  string `xml:"title,attr"`
+		Author string `xml:"author,attr"`
+		ID     string `xml:"owi,attr"`
+	} `xml:"work"`
+	Classification struct {
+		MostPopular string `xml:"sfa,attr"`
+	} `xml:"recomendations>ddc>mostPopular"`
+}
+
+func find(id string) (ClassifyBookResponse, error) {
+	var c ClassifyBookResponse
+	body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?&summary=true&owi=" + url.QueryEscape(id))
+
+	if err != nil {
+		return ClassifyBookResponse{}, err
+	}
+
+	err = xml.Unmarshal(body, &c)
+	return c, err
+}
+
 func search(query string) ([]SearchResult, error) {
+
+	var c ClassifySearchResponse
+	body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query))
+
+	if err != nil {
+		return []SearchResult{}, err
+	}
+	err = xml.Unmarshal(body, &c)
+	return c.Results, err
+}
+
+func classifyAPI(url string) ([]byte, error) {
+
 	var resp *http.Response
 	var err error
 
-	if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err != nil {
-		return []SearchResult{}, err
+	if resp, err = http.Get(url); err != nil {
+		return []byte{}, err
 	}
 
 	defer resp.Body.Close()
 
-	var body []byte
-	if body, err = ioutil.ReadAll(resp.Body); err != nil {
-		return []SearchResult{}, err
-	}
-
-	var c ClassifySearchResponse
-	err = xml.Unmarshal(body, &c)
-
-	return c.Results, err
+	return ioutil.ReadAll(resp.Body)
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,32 @@
 # How to setup a webserver in go
 
+Tutorial: https://www.lynda.com/Go-tutorials/Replacing-default-template-engine/516402/516855-4.html
+
+## run the server
+
+go run main.go
+
+http://localhost:8080/
+
+## setup webserver negroni
+
+go get github.com/codegangsta/negroni
 
 ## Setup db
 
+$ go get github.com/mattn/go-sqlite3
+
 $ sqlite3 dev.db
+
+sqlite> .schema
+
+CREATE TABLE books(
+pk integer primary key autoincrement,
+title text,
+author text,
+id text,
+classification text);
+
+sqlite> select * from books;
+
+sqlite> .exit
